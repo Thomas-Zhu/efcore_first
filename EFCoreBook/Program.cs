ddf7f54ce9586b67d4454921db33349334b263b2
托马斯 <gangzhucoll1990@live.com>
2021-03-17 21:01:48 +0800

    first commit

---
+++ EFCoreBook/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace EFCoreBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                Console.WriteLine("add blog");
+                db.Add(new Blog
+                {
+                    Url="http://blogs.msdn.com/dotnet"
+                });
+                db.SaveChanges();
+                Console.WriteLine("search blog");
+                var blog = db.Blogs.OrderBy(b => b.BlogId).First();
+                Console.WriteLine("edit blog");
+                blog.Url = "xxxxxxx";
+                blog.Posts.Add(new Post
+                {
+                    Title = "hello world",
+                    Content = "first code"
+                }); ;
+                db.SaveChanges();
+                Console.WriteLine("delete blog");
+                db.Remove(blog);
+                db.SaveChanges();
+            }
+        }
+    }
+}
